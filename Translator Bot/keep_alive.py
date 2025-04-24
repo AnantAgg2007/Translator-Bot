0e01b5e42262f806eba7820eed5fda1358b2e1a0
Anant Aggarwal <41811070-anantagg20072@users.noreply.replit.com>
2025-04-24 22:28:28 +0000

    Updated keep_alive script to run in threaded mode

--- Translator Bot/keep_alive.py
+++ Translator Bot/keep_alive.py
@@ -6,13 +6,15 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "I'm alive!"
+    return "I'm alive!"  # Simple route to keep the server alive
 
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080,
+            threaded=True)  # Enable threaded mode for better performance
 
 
 def keep_alive():
     t = Thread(target=run)
+    t.daemon = True  # Make sure the thread dies when the main program ends
     t.start()
